8683994ab6b442fb35252234eae1d3f1d075ba0e
Vishnu M K <mkv1722@gmail.com>
2023-07-18 13:06:59 +0530

    ADD : loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,16 @@
 function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="loader py-10 h-20">
+      <div className="duo duo1">
+        <div className="dot dot-a"></div>
+        <div className="dot dot-b"></div>
+      </div>
+      <div className="duo duo2">
+        <div className="dot dot-a"></div>
+        <div className="dot dot-b"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
